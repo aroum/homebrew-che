f5f9592bb204b02b030790dbaff63a7cd1eccf8d
aroum <aroum24@gmail.com>
2026-08-08 08:57:55 +0300

    che 26.8.7
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/che.rb
+++ Formula/che.rb
@@ -1,12 +1,12 @@
 class Che < Formula
   desc "Dual-pane terminal file manager written in Rust"
   homepage "https://github.com/aroum/che"
-  version "0.2.0"
+  version "26.8.7"
   license "MIT"
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/aroum/che/releases/download/v0.2.0/che-v0.2.0-aarch64-apple-darwin.zip"
-    sha256 "3a8a83e3f13d468fd4c3b4a5c0e541540b3aefae5cd8657b6b6e2f666ff009dc"
+    url "https://github.com/aroum/che/releases/download/v26.8.7/che-v26.8.7-x86_64-apple-darwin.zip"
+    sha256 "0b209ce09f32ddf205d8c9e3da5a1379ba6abbfd5290847cf833ea42efd019ab"
   elsif OS.mac? && Hardware::CPU.intel?
     url "https://github.com/aroum/che/releases/download/v0.2.0/che-v0.2.0-x86_64-apple-darwin.zip"
     sha256 "b6f53e24a666f0eb34a16ca5a8349a590b419e21b61c0a8b471587e95a9f5370"
